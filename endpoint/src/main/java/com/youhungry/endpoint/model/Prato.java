2e08b2462f197531e92c807b0e45479b38a863e2
Olivera <olivera.jr18@gmail.com>
2021-09-14 16:17:30 -0300

    quarto commit

--- endpoint/src/main/java/com/youhungry/endpoint/model/Prato.java
+++ endpoint/src/main/java/com/youhungry/endpoint/model/Prato.java
@@ -2,9 +2,99 @@ package com.youhungry.endpoint.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 public class Prato {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int idprato;
+
+    @Column(length = 40, nullable = false)
+    private String nome;
+    @Column(length = 90, nullable = false)
+    private String descricao;
+    @OneToOne
+    @JoinColumn(name = "idrestaurante",nullable=false)
+    private Restaurante restaurante;
+    @Column @Temporal(TemporalType.TIME) // HH:mm:ss
+    private Date tempoParaPreparo;
+    @Column
+    private String acompanhamento;
+    @Column
+    private double preco;
+    @Column
+    private String url;
+
+    public Prato(String nome, String descricao, Restaurante restaurante) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.restaurante = restaurante;
+    }
+
+    public int getIdprato() {
+        return idprato;
+    }
+
+    public void setIdprato(int idprato) {
+        this.idprato = idprato;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Restaurante getRestaurante() {
+        return restaurante;
+    }
+
+    public void setRestaurante(Restaurante restaurante) {
+        this.restaurante = restaurante;
+    }
+
+    public Date getTempoParaPreparo() {
+        return tempoParaPreparo;
+    }
+
+    public void setTempoParaPreparo(Date tempoParaPreparo) {
+        this.tempoParaPreparo = tempoParaPreparo;
+    }
+
+    public String getAcompanhamento() {
+        return acompanhamento;
+    }
+
+    public void setAcompanhamento(String acompanhamento) {
+        this.acompanhamento = acompanhamento;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
--- endpoint/src/main/java/com/youhungry/endpoint/model/Restaurante.java
+++ endpoint/src/main/java/com/youhungry/endpoint/model/Restaurante.java
@@ -6,45 +6,46 @@ import javax.persistence.*;
 
 @Entity
 @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
-public class Usuario {
-
+public class Restaurante {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
-    private int idusuario;
+    private int idrestaurante;
 
-    @Column(length = 60, nullable = false)
-    private String email;
-    @Column(length = 30, nullable = false)
-    private String password;
     @Column(length = 60, nullable = false)
     private String nome;
-    @Column
+    @Column(length = 30, nullable = false)
+    private String password;
+    @Column(length = 80)
+    private String especialidade;
+    @Column(length = 80)
     private String endereco;
-    @Column
+    @Column(length = 40)
     private String cidade;
-    @Column
+    @Column(length = 20)
     private String cep;
+    @Column(length = 40, nullable = false)
+    private String cnpj;
 
-    public Usuario(String email, String password, String nome) {
-        this.email = email;
-        this.password = password;
+    public Restaurante(String nome, String password, String cnpj) {
         this.nome = nome;
+        this.password = password;
+        this.cnpj = cnpj;
     }
 
-    public int getIdusuario() {
-        return idusuario;
+    public int getIdrestaurante() {
+        return idrestaurante;
     }
 
-    public void setIdusuario(int idusuario) {
-        this.idusuario = idusuario;
+    public void setIdrestaurante(int idrestaurante) {
+        this.idrestaurante = idrestaurante;
     }
 
-    public String getEmail() {
-        return email;
+    public String getNome() {
+        return nome;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public String getPassword() {
@@ -55,12 +56,12 @@ public class Usuario {
         this.password = password;
     }
 
-    public String getNome() {
-        return nome;
+    public String getEspecialidade() {
+        return especialidade;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
     }
 
     public String getEndereco() {
@@ -85,5 +86,13 @@ public class Usuario {
 
     public void setCep(String cep) {
         this.cep = cep;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
     }
 }
